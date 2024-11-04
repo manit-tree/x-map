af18cd89aec18a4758ea94a8ab617c46a624049c
8columns <manit.tree@gmail.com>
2024-11-04 14:38:06 +0700

    update

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    map.setPopup({
-        title: 'บ้านนิรมิตร',
-        address: '27/18 ต.บ้านใหม่ อ.ปากเกร็ด จ.นนทบุรี  11120',
-        tel: '080-597-9990',
-        email: '8columns@gmail.com'
+    map.addEventListener('ready', () => {
+        map.setPopup({
+            title: 'บ้านนิรมิตร',
+            address: '27/18 ต.บ้านใหม่ อ.ปากเกร็ด จ.นนทบุรี  11120',
+            tel: '080-597-9990',
+            email: '8columns@gmail.com'
+        })
+    })
+
+    test.addEventListener('click', () => {
+        map.setLocation('13.854442619326525', '100.58495218973192').then(() => {
+            map.setPopup({
+                title: 'บ้านนิรมิตร xx',
+                address: '27/18 ต.บ้านใหม่ อ.ปากเกร็ด จ.นนทบุรี  11120',
+                tel: '080-597-9990',
+                email: '8columns@gmail.com'
+            })
+
+        })
     })
 })
--- src/index.ts
+++ src/index.ts
@@ -12,21 +12,21 @@ interface info {
     email?:string;
 }
 
+type lat = number | string;
+type lng = number | string;
+
 @customElement('x-map')
 export class TMap extends LitElement {
-    @property()
-    lat = '13.926841663282733';
-
-    @property()
-    lng = '100.52599680608748';
-
-    @property()
-    zoom = 16;
+    @property() lat = '13.926841663282733';
+    @property() lng = '100.52599680608748';
+    @property() zoom = 16;
 
     map;
     marker;
+    popup;
+    count:number = 0;
 
-    init() {
+    init():void {
         const location = [this.lng, this.lat];
         let map_container = this.renderRoot.querySelector('.map-container');
 
@@ -45,15 +45,67 @@ export class TMap extends LitElement {
 
         this.map.on('load', () => {
             const nav = new maplibregl.NavigationControl({showCompass: false});
-            this.map.addControl(nav, 'top-right');
 
-            this.marker.setLngLat(location);
-            this.marker.addTo(this.map);
+            this.map.addControl(nav, 'top-right');
+            this.addMarker();
+
+            this.fadeOut(this.renderRoot.querySelector('.overlay'), 600, () => {
+                this.renderRoot.classList.add('ready');
+                this.renderRoot.dispatchEvent(new CustomEvent("ready"));
+            })
+        })
+    }
+
+    addMarker():void {
+        if (this.marker) {
+            this.marker.remove();
+        }
+
+        this.marker = new maplibregl.Marker({color: 'red'})
+        this.marker.setLngLat([this.lng, this.lat]);
+        this.marker.addTo(this.map);
+    }
+
+    fadeOut(el:HTMLElement, duration:number = 600, cb?:func) {
+        let keyframes = [
+            {"opacity": 1},
+            {"opacity": 0}
+        ]
+
+        let settings = {
+            duration: duration,
+            iterations: 1,
+            fill: 'both'
+        }
+
+        let animation = el.animate(keyframes, settings);  
+
+        if (typeof cb === 'function') {
+            animation.addEventListener('finish', evt => {
+                cb();
+            })
+        }
+
+        return this;      
+    }
+
+    setLocation(lat:lat, lng:lng) {
+        return new Promise(resolve => {
+            this.renderRoot.setAttribute('lat', lat);
+            this.renderRoot.setAttribute('lng', lng);
+            
+            setTimeout(() => {
+                resolve();
+            }, 1500)            
         })
     }
 
     setPopup(info:info) {
         if (!info) return;
+
+        if (this.popup) {
+            this.popup.remove();
+        }
 
         let sb = new Array();
 
@@ -73,23 +125,42 @@ export class TMap extends LitElement {
         sb.push('<p class="direction">' + icons['car'] + ' <a href="https://www.google.com/maps/dir/?api=1&destination=' + this.lat + ',' + this.lng + '" target="_blank">วิธีการเดินทาง</a></p>');
         sb.push('</div>');
 
-        this.marker.setPopup(new maplibregl.Popup().setHTML(sb.join('')));
+        this.popup = new maplibregl.Popup().setHTML(sb.join(''));
+        this.marker.setPopup(this.popup);    
     }
 
-    createRenderRoot() {
+    createRenderRoot():void {
         return this;
     }
 
-    render() {
+    render():void {
         return (
             html`
                 <div class="map-container"></div>
+                <div class="overlay">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 24 24" style="fill: currentColor;transform: ;msFilter:;"><path d="M12 2C7.589 2 4 5.589 4 9.995 3.971 16.44 11.696 21.784 12 22c0 0 8.029-5.56 8-12 0-4.411-3.589-8-8-8zm0 12c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4z"></path></svg>                        
+                </div>
             `
         )
     }
 
-    updated() {
+    firstUpdated():void {
         this.init();
+    }
+
+    updated():void {
+        if (this.count) {
+            const location = [this.lng, this.lat];
+            this.map.panTo(location);
+
+            setTimeout(() => {
+                this.addMarker();
+            }, 1000)
+
+            return;
+        }
+
+        this.count++;
     }
 }
 
